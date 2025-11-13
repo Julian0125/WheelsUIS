3b1983a00c4deba7cb6368166e5262ec21012a05
jerson <mantillajerson2@gmail.com>
2025-11-12 22:20:03 -0500

    data

--- GestionWheels/src/main/java/com/example/demo/DTO/RutasPredefinidas.java
+++ GestionWheels/src/main/java/com/example/demo/DTO/RutasPredefinidas.java
@@ -1,7 +1,9 @@
 package com.example.demo.DTO;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.time.LocalDateTime;
+@Data
 
 @AllArgsConstructor
 public class RutasPredefinidas {
